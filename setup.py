f49249d94c5660870cb96ccdaec43014c2554bc1
SutandoTsukai181 <mosamaeldeeb@gmail.com>
2021-05-10 14:17:55 +0300

    Add PyPi dependencies

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+setup(
+  name = 'binary_reader',
+  packages = ['binary_reader'],
+  version = '1.1',
+  license='MIT',
+  description = 'A python module for basic binary file IO.',
+  author = 'SutandoTsukai181',
+  author_email = 'mosamaeldeeb@gmail.com',
+  url = 'https://github.com/SutandoTsukai181/PyBinaryReader',
+  download_url = 'https://github.com/SutandoTsukai181/PyBinaryReader/archive/refs/tags/v1.1.tar.gz',
+  keywords = ['BINARY', 'IO'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
