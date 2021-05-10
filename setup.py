8b5d8ba94cc64f958772dcdf5bdcd7226a27254a
SutandoTsukai181 <mosamaeldeeb@gmail.com>
2021-05-10 14:26:22 +0300

    Update PyPi package

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'binary_reader',
   packages = ['binary_reader'],
-  version = '1.1',
+  version = '1.2',
   license='MIT',
   description = 'A python module for basic binary file IO.',
   author = 'SutandoTsukai181',
   author_email = 'mosamaeldeeb@gmail.com',
   url = 'https://github.com/SutandoTsukai181/PyBinaryReader',
-  download_url = 'https://github.com/SutandoTsukai181/PyBinaryReader/archive/refs/tags/v1.1.tar.gz',
+  download_url = 'https://github.com/SutandoTsukai181/PyBinaryReader/archive/refs/tags/v1.2.tar.gz',
   keywords = ['BINARY', 'IO'],
   install_requires=[],
   classifiers=[
